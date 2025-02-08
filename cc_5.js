de6f9bd9b0c1d067c4e4c77dd7ed8a4b1642c1d2
rhedgesusf <rhedges1@usf.edu>
2025-02-08 06:43:26 -0500

    Finalized comments

--- cc_5.js
+++ cc_5.js
@@ -5,6 +5,7 @@
 console.log("--------------------------------------");
 console.log("Task 1: Object Properties");
 
+// initialize customer object
 let customer = {
     name: "John Doe",
     age: 35,
@@ -23,6 +24,7 @@ for (let property in customer) {
 console.log("--------------------------------------");
 console.log("Task 2: Object Methods");
 
+// initialize order object
 let order = {
     orderId: 12345,
     totalAmount: 150,
@@ -32,6 +34,7 @@ let order = {
     }
 };
 
+// invoke object function to display order details
 order.displayOrder();
 
 ////////////////////////////////
@@ -41,19 +44,23 @@ order.displayOrder();
 console.log("--------------------------------------");
 console.log("Task 3: Array Manipulation");
 
+// initialize cart array
 let cartItems = ["Water", "Tea", "Coffee"];
-
 console.log(`Initial Items in Cart: ${cartItems}`);
 
+// push an item to end of cart
 cartItems.push("Milk");
 console.log(`Added Milk: ${cartItems}`);
 
+// remove an item at end of cart
 cartItems.pop();
 console.log(`Got rid of milk: ${cartItems}`);
 
+// add two items to front of cart
 cartItems.unshift("Juice", "Wine");
 console.log(`Added Juice & Wine: ${cartItems}`);
 
+// remove item at front of cart
 cartItems.shift();
 console.log(`Final items in Cart: ${cartItems}`);
 
@@ -64,9 +71,11 @@ console.log(`Final items in Cart: ${cartItems}`);
 console.log("--------------------------------------");
 console.log("Task 4: Map Method");
 
+// initialize price array
 let prices = [100, 200, 300];
 console.log("Initial Prices:", prices);
 
+// use map() function to apply discount to each item in array
 let discountPrices = prices.map(num => num * .90);
 console.log("Applied 10% Discount:", discountPrices);
 
@@ -77,9 +86,11 @@ console.log("Applied 10% Discount:", discountPrices);
 console.log("--------------------------------------");
 console.log("Task 5: Filter Method");
 
+// intialize inventory array
 let inventory = [5, 0, 12, 8, 0];
 console.log("Initial Inventory:", inventory);
 
+// use filter() function to remove all zero quantity items
 let inventoryLimit = inventory.filter(num => num > 0);
 console.log("Removed Orders w/ Zero Stock:", inventoryLimit);
 
@@ -90,9 +101,11 @@ console.log("Removed Orders w/ Zero Stock:", inventoryLimit);
 console.log("--------------------------------------");
 console.log("Task 6: Reduce Method");
 
+// initialize sales array
 let sales = [500, 300, 200, 400];
 console.log("Initial Sales:", sales);
 
+// use reduce() function to add up all sales to calculate sum
 let salesRevenue = sales.reduce((sum, sale) => sum + sale, 0);
 console.log("Total Sales:", salesRevenue);
 
@@ -103,11 +116,13 @@ console.log("Total Sales:", salesRevenue);
 console.log("--------------------------------------");
 console.log("Task 7: find() Method");
 
+// initialize customer array
 let customers = ["Alice", "Bob", "Charlie", "David"];
 console.log("Initial Customers:", customers);
 
 let searchName = "Charlie";
 
+// use find() method to determine if search value is in array else not defined
 let found = customers.find((element) => element == searchName);
 
 console.log("Found Customer:", found);
@@ -119,14 +134,16 @@ console.log("Found Customer:", found);
 console.log("--------------------------------------");
 console.log("Task 8: Tax Calculation");
 
+// create function to calculateTax based on amount and taxrate
 function calculateTax(amount, taxRate) {
     return amount * taxRate;
 }
- let number = 100;
- let taxRate = .07;
 
- console.log(`For purchase of $${number} at 7% tax rate`);
- console.log(`Tax Amount: $${calculateTax(number, taxRate).toFixed(2)}`);
+let number = 100;
+let taxRate = .07;
+
+console.log(`For purchase of $${number} at 7% tax rate`);
+console.log(`Tax Amount: $${calculateTax(number, taxRate).toFixed(2)}`);
 
  ////////////////////////////////
 // Task 9: Function Expression //
@@ -135,10 +152,15 @@ function calculateTax(amount, taxRate) {
 console.log("--------------------------------------");
 console.log("Task 9: Function Expression");
 
+// create function to calculate discount price
+// input parameters are": price and discount percentage (non-decimal)
 const applyDiscount  = function(price, discountPercent) {
     return price - (price * discountPercent / 100);
 };
+
+// calculate the discount price
 let discountedPrice = applyDiscount(896, 15);
+
 console.log(`Initial Price: $896`)
 console.log(`15% Discounted Price: $${discountedPrice}`);
 
@@ -147,7 +169,7 @@ console.log(`15% Discounted Price: $${discountedPrice}`);
 /////////////////////////////
 
 console.log("--------------------------------------");
-console.log("Task 9: Arrow Function");
+console.log("Task 10: Arrow Function");
 
 // Create function to calculate points based off of purchase amount
 // Points are rounded down to nearest whole number
